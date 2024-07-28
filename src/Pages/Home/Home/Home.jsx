c44ce8b4e29acd250e9821b6aa37b0cc35582142
shuvo sutradhor <shuvosd3837@gmail.com>
2024-07-28 13:07:14 +0600

    added about section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
+import About from "../About/About";
 import Bannar from "../Bannar/Bannar";
 
 
 const Home = () => {
     return (
         <div>
-            <Bannar></Bannar>
+            <Bannar></Bannar>,
+            <About></About>
         </div>
     );
 };
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,20 @@
+
+
+const Services = () => {
+    return (
+        <div>
+            <div>
+                <small>Service</small>
+                <h2>Our Service Area</h2>
+                <p>
+                the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                </p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Services;
